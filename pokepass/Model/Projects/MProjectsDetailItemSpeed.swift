f36e45586ea735b4de49374e73d9202b2caa645d
vauxhall <zero.griffin@gmail.com>
2016-08-15 20:52:05 -0500

    factory with cell height

--- pokepass/Model/Projects/MProjectsDetailItemSpeed.swift
+++ pokepass/Model/Projects/MProjectsDetailItemSpeed.swift
@@ -1,15 +1,19 @@
-import Foundation
+import UIKit
 
 class MProjectsDetailItemSpeed:MProjectsDetailItem
 {
     let index:Int
     let title:String
     let maxDistance:Double
+    private let kCellHeight:CGFloat = 48
     
     init(index:Int, title:String, maxDistance:Double)
     {
         self.index = index
         self.title = title
         self.maxDistance = maxDistance
+        let reusableIdentifier:String = VProjectsDetailCellDistance.reusableIdentifier()
+        
+        super.init(reusableIdentifier:reusableIdentifier, cellHeight:kCellHeight)
     }
 }
