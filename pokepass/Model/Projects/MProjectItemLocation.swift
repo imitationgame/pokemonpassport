e2bcb20a6b5970b79f36057a5170f4e2eecbc65d
vauxhall <zero.griffin@gmail.com>
2016-08-16 12:01:50 -0500

    item print

--- pokepass/Model/Projects/MProjectItemLocation.swift
+++ pokepass/Model/Projects/MProjectItemLocation.swift
@@ -4,10 +4,20 @@ class MProjectItemLocation
 {
     let latitude:Double
     let longitude:Double
+    private let kPrint:String = "<wpt lat=\"%@\" lon=\"%@\" />"
     
     init(latitude:Double, longitude:Double)
     {
         self.latitude = latitude
         self.longitude = longitude
+    }
+    
+    //MARK: public
+    
+    func print() -> String
+    {
+        let string:String = String(format:kPrint, latitude, longitude)
+        
+        return string
     }
 }
