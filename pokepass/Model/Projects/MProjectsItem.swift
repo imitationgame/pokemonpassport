2bb6dde1361e62aa5af146775883400a814a9267
vauxhall <zero.griffin@gmail.com>
2016-08-16 10:41:26 -0500

    parsing locations

--- pokepass/Model/Projects/MProjectsItem.swift
+++ pokepass/Model/Projects/MProjectsItem.swift
@@ -4,7 +4,7 @@ class MProjectsItem
 {
     let name:String
     let model:DPokePassProject
-    var locations:[]
+    var locations:[MProjectItemLocation]?
     
     init(model:DPokePassProject)
     {
@@ -12,10 +12,69 @@ class MProjectsItem
         self.model = model
     }
     
+    //MARK: private
+    
+    private func itemForLocation(location:DPokePassLocation)
+    {
+        let latitude:Double = location.latitude
+        let longitude:Double = location.longitude
+        
+        let item:MProjectItemLocation = MProjectItemLocation(latitude:latitude, longitude:longitude)
+        locations!.append(item)
+    }
+    
+    private func pointsBetweenLocations(index:Int, locationA:DPokePassLocation?, locationB:DPokePassLocation?, maxDistance:Double)
+    {
+        if locationB != nil
+        {
+            if locationA == nil
+            {
+                itemForLocation(locationB!)
+            }
+            else
+            {
+                
+            }
+            
+            let newLocationA:DPokePassLocation? = locationB
+            let newLocationB:DPokePassLocation?
+            let countLocations:Int = model.projectLocations.count
+            let nextIndex:Int = index + 1
+            
+            if nextIndex < countLocations
+            {
+                newLocationB = model.projectLocations[nextIndex]
+            }
+            else
+            {
+                newLocationB = nil
+            }
+            
+            pointsBetweenLocations(nextIndex, locationA:newLocationA, locationB:newLocationA, maxDistance:maxDistance)
+        }
+    }
+    
     //MARK: public
     
-    func getLocations()
+    func getLocations(maxDistance:Double)
     {
-        for
+        locations = []
+        var previousDbLocation:DPokePassLocation?
+        
+        for dbLocation:DPokePassLocation in model.projectLocations
+        {
+            let latitude:Double = dbLocation.latitude
+            let longitude:Double = dbLocation.longitude
+            
+            if previousDbLocation == nil
+            {
+                previousDbLocation = dbLocation
+                
+            }
+            else
+            {
+                
+            }
+        }
     }
 }
