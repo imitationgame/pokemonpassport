e353f5379306fd536473d88cb373fcb8d9bbd768
vauxhall <zero.griffin@gmail.com>
2016-08-17 17:45:54 -0500

    added hidden button

--- pokepass/View/Create/VCreateFinder.swift
+++ pokepass/View/Create/VCreateFinder.swift
@@ -11,16 +11,31 @@ class VCreateFinder:UIView
         backgroundColor = UIColor.whiteColor()
         translatesAutoresizingMaskIntoConstraints = false
         
+        let image:UIImageView = UIImageView()
+        image.image = UIImage(named:"search")
+        image.userInteractionEnabled = false
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        image.contentMode = UIViewContentMode.Center
+        
         let border:UIView = UIView()
         border.backgroundColor = UIColor.main()
         border.userInteractionEnabled = false
         border.translatesAutoresizingMaskIntoConstraints = false
         border.clipsToBounds = true
         
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action:#selector(self.actionButton(sender:)), forControlEvents:UIControlEvents.TouchUpInside)
+        
         addSubview(border)
+        addSubview(image)
+        addSubview(button)
         
         let views:[String:AnyObject] = [
-            "border":border]
+            "border":border,
+            "image":image,
+            "button":button]
         
         let metrics:[String:AnyObject] = [:]
         
@@ -34,5 +49,22 @@ class VCreateFinder:UIView
             options:[],
             metrics:metrics,
             views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-5-[image(20)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[image]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
+    
+    //MARK: actions
+    
+    func actionButton(sender button:UIButton)
+    {
+        
     }
 }
