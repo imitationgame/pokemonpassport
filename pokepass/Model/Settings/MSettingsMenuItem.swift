793fae40bf17dd9d1373f61b50fec7b780db61a3
vauxhall <zero.griffin@gmail.com>
2016-08-17 12:58:35 -0500

    factory items

--- pokepass/Model/Settings/MSettingsMenuItem.swift
+++ pokepass/Model/Settings/MSettingsMenuItem.swift
@@ -5,6 +5,13 @@ class MSettingsMenuItem
     weak var cell:VSettingsCell!
     weak var controller:CSettings!
     
+    class func Metrics() -> MSettingsMenuItemMetrics
+    {
+        let item:MSettingsMenuItemMetrics = MSettingsMenuItemMetrics()
+        
+        return item
+    }
+    
     //MARK: public
     
     func config(cell:VSettingsCell, controller:CSettings)
