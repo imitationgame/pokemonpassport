9e24df03546b8674927314776656709b6bcd270c
vauxhall <zero.griffin@gmail.com>
2016-08-17 12:28:05 -0500

    controller settings uses model

--- pokepass/Controller/Settings/CSettings.swift
+++ pokepass/Controller/Settings/CSettings.swift
@@ -2,7 +2,20 @@ import UIKit
 
 class CSettings:CMainController
 {
+    let model:MSettingsMenu
     weak var viewSettings:VSettings!
+ 
+    init()
+    {
+        model = MSettingsMenu()
+        
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
     
     override func loadView()
     {
