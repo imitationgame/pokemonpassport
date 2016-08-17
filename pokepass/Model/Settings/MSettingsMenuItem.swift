bf5ac4b0561adb26596c4495987724bc8872bd1c
vauxhall <zero.griffin@gmail.com>
2016-08-17 12:37:14 -0500

    public functions for settings cell

--- pokepass/Model/Settings/MSettingsMenuItem.swift
+++ pokepass/Model/Settings/MSettingsMenuItem.swift
@@ -1,6 +1,19 @@
-import Foundation
+import UIKit
 
 class MSettingsMenuItem
 {
+    weak var cell:VSettingsCell!
+    weak var controller:CSettings!
     
+    //MARK: public
+    
+    func config(cell:VSettingsCell, controller:CSettings)
+    {
+        self.cell = cell
+        self.controller = controller
+    }
+    
+    func selected()
+    {
+    }
 }
