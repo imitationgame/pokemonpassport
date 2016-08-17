46bcc1b4773d9a3cb55d1d2dba1ddf7b6760fd96
vauxhall <zero.griffin@gmail.com>
2016-08-17 13:51:16 -0500

    collection in settings

--- pokepass/View/Settings/VSettings.swift
+++ pokepass/View/Settings/VSettings.swift
@@ -3,7 +3,7 @@ import UIKit
 class VSettings:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     weak var controller:CSettings!
-    weak var collection:UICollectionView!
+    private let kCollectionBottom:CGFloat = 40
     
     convenience init(controller:CSettings)
     {
@@ -14,11 +14,44 @@ class VSettings:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UI
         translatesAutoresizingMaskIntoConstraints = false
         
         let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSizeZero
+        flow.footerReferenceSize = CGSizeZero
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.Vertical
+        flow.sectionInset = UIEdgeInsetsMake(0, 0, kCollectionBottom, 0)
         
         let collection:UICollectionView = UICollectionView(frame:CGRectZero, collectionViewLayout:flow)
-        self.collection = collection
+        collection.clipsToBounds = true
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.backgroundColor = UIColor.clearColor()
+        collection.showsVerticalScrollIndicator = false
+        collection.showsHorizontalScrollIndicator = false
+        collection.alwaysBounceVertical = true
+        collection.delegate = self
+        collection.dataSource = self
+        collection.registerClass(
+            VSettingsCellMetrics.self,
+            forCellWithReuseIdentifier:
+            VSettingsCellMetrics.reusableIdentifier())
         
         addSubview(collection)
+        
+        let views:[String:AnyObject] = [
+            "collection":collection]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[collection]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[collection]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     //MARK: private
@@ -31,6 +64,15 @@ class VSettings:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UI
     }
     
     //MARK: col del
+    
+    func collectionView(collectionView:UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAtIndexPath indexPath:NSIndexPath) -> CGSize
+    {
+        let item:MSettingsMenuItem = modelAtIndex(indexPath)
+        let width:CGFloat = collectionView.bounds.maxX
+        let size:CGSize = CGSizeMake(width, item.cellHeight)
+        
+        return size
+    }
     
     func numberOfSectionsInCollectionView(collectionView:UICollectionView) -> Int
     {
@@ -52,7 +94,7 @@ class VSettings:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UI
             forIndexPath:
             indexPath) as! VSettingsCell
         
-        item.config(cell, controller:self)
+        item.config(cell, controller:controller)
         
         return cell
     }
