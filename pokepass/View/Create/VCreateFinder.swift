ad50ffa5ef3517230a4668a3ba0c902bfe7b7c2a
vauxhall <zero.griffin@gmail.com>
2016-08-17 18:54:11 -0500

    added button search

--- pokepass/View/Create/VCreateFinder.swift
+++ pokepass/View/Create/VCreateFinder.swift
@@ -1,14 +1,15 @@
 import UIKit
 
-class VCreateFinder:UIView
+class VCreateFinder:UIView, UITextFieldDelegate
 {
     weak var controller:CCreate!
+    weak var field:UITextField!
     
     convenience init(controller:CCreate)
     {
         self.init()
         clipsToBounds = true
-        backgroundColor = UIColor.whiteColor()
+        backgroundColor = UIColor(white:0.95, alpha:1)
         translatesAutoresizingMaskIntoConstraints = false
         
         let image:UIImageView = UIImageView()
@@ -28,14 +29,43 @@ class VCreateFinder:UIView
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action:#selector(self.actionButton(sender:)), forControlEvents:UIControlEvents.TouchUpInside)
         
+        let buttonSearch:UIButton = UIButton()
+        buttonSearch.translatesAutoresizingMaskIntoConstraints = false
+        buttonSearch.backgroundColor = UIColor.main()
+        buttonSearch.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
+        buttonSearch.setTitleColor(UIColor(white:1, alpha:0.2), forState:UIControlState.Highlighted)
+        buttonSearch.setTitle(NSLocalizedString("VCreateFinder_searchButton", comment:""), forState:UIControlState.Normal)
+        
+        let field:UITextField = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.clipsToBounds = true
+        field.backgroundColor = UIColor.clearColor()
+        field.borderStyle = UITextBorderStyle.None
+        field.font = UIFont.regular(16)
+        field.textColor = UIColor.blackColor()
+        field.tintColor = UIColor.blackColor()
+        field.returnKeyType = UIReturnKeyType.Search
+        field.keyboardAppearance = UIKeyboardAppearance.Light
+        field.autocorrectionType = UITextAutocorrectionType.No
+        field.spellCheckingType = UITextSpellCheckingType.No
+        field.autocapitalizationType = UITextAutocapitalizationType.Words
+        field.clearButtonMode = UITextFieldViewMode.WhileEditing
+        field.placeholder = NSLocalizedString("VCreateFinder_fieldPlaceholder", comment:"")
+        field.delegate = self
+        self.field = field
+        
         addSubview(border)
         addSubview(image)
         addSubview(button)
+        addSubview(field)
+        addSubview(buttonSearch)
         
         let views:[String:AnyObject] = [
             "border":border,
             "image":image,
-            "button":button]
+            "button":button,
+            "buttonSearch":buttonSearch,
+            "field":field]
         
         let metrics:[String:AnyObject] = [:]
         
@@ -45,7 +75,12 @@ class VCreateFinder:UIView
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[border(1)]-0-|",
+            "H:|-0-[button]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-30-[field]-0-[buttonSearch(60)]-0-|",
             options:[],
             metrics:metrics,
             views:views))
@@ -55,7 +90,27 @@ class VCreateFinder:UIView
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[border(1)]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
             "V:|-0-[image]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[button]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[field]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[buttonSearch]-0-|",
             options:[],
             metrics:metrics,
             views:views))
@@ -65,6 +120,32 @@ class VCreateFinder:UIView
     
     func actionButton(sender button:UIButton)
     {
+        field.becomeFirstResponder()
+    }
+    
+    //MARK: field delegate
+    
+    func textField(textField:UITextField, shouldChangeCharactersInRange range:NSRange, replacementString string:String) -> Bool
+    {
+        let nsString:NSString = textField.text! as NSString
+        let query:String = textField.text!.stringByReplacingCharactersInRange(range, withString:string)
         
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0))
+        { [weak self] in
+            
+            let newString:String = nsString.stringByReplacingCharactersInRange(range, withString:string)
+            self?.searchQuery(newString)
+        }
+        
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(textField:UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        
+        return true
     }
 }
