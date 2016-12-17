4620084e121a90882dfe8237ac528e8e58bcc0fb
vauxhall <zero.griffin@gmail.com>
2016-12-17 15:58:52 -0600

    extension notifications and reusable view

--- pokepass/Category/CategoryNSNotification.swift
+++ pokepass/Category/CategoryNSNotification.swift
@@ -2,20 +2,5 @@ import Foundation
 
 extension Notification
 {
-    static func observeUserSynced(_ receiver:AnyObject, sel:Selector)
-    {
-        NotificationCenter.default.addObserver(receiver, selector:sel, name:NSNotification.Name(rawValue: userSyncedName()), object:nil)
-    }
-    
-    static func postUserSynced()
-    {
-        NotificationCenter.default.post(name: Notification.Name(rawValue: userSyncedName()), object:nil)
-    }
-    
-    //MARK: private
-    
-    fileprivate static func userSyncedName() -> String
-    {
-        return "userSynced"
-    }
+    static let userSynced:Notification.Name = Notification.Name("userSynced")
 }
