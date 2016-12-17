4944f8a5c3d0c674b71d5f043dc5fdd70daa6c36
vauxhall <zero.griffin@gmail.com>
2016-12-17 15:50:29 -0600

    app directory

--- pokepass/Category/CategoryNSFileManager.swift
+++ pokepass/Category/CategoryNSFileManager.swift
@@ -2,11 +2,16 @@ import Foundation
 
 extension FileManager
 {
-    class func appDirectory() -> URL
+    open class var appDirectory:URL
     {
-        let urls:[URL] = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in:FileManager.SearchPathDomainMask.userDomainMask)
-        let appDirectory:URL = urls.last!
-        
-        return appDirectory
+        get
+        {
+            let urls:[URL] = FileManager.default.urls(
+                for:FileManager.SearchPathDirectory.documentDirectory,
+                in:FileManager.SearchPathDomainMask.userDomainMask)
+            let appDirectory:URL = urls.last!
+            
+            return appDirectory
+        }
     }
 }
