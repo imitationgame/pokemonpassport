4236b1bfd2aaf19c45d4272ba2f03d455cd68498
vauxhall <zero.griffin@gmail.com>
2016-12-18 01:58:59 -0600

    added collection view history

--- pokepass/View/Create/VCreateHistory.swift
+++ pokepass/View/Create/VCreateHistory.swift
@@ -2,5 +2,52 @@ import UIKit
 
 class VCreateHistory:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
+    private weak var controller:CCreate!
+    private weak var collectionView:UICollectionView!
     
+    convenience init(controller:CCreate)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+        
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        
+        let collectionView:UICollectionView = UICollectionView(
+            frame:CGRect.zero,
+            collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.alwaysBounceHorizontal = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            VCreateHistoryCell.self,
+            forCellWithReuseIdentifier:
+            VCreateHistoryCell.reusableIdentifier)
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
+        
+        let views:[String:UIView] = [
+            "collectionView":collectionView]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
 }
