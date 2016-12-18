d6b2dd36144d5a1bd251239d80eaa06e4c8531ad
vauxhall <zero.griffin@gmail.com>
2016-12-18 14:04:43 -0600

    label points

--- pokepass/View/Projects/VProjectsCell.swift
+++ pokepass/View/Projects/VProjectsCell.swift
@@ -2,10 +2,18 @@ import UIKit
 
 class VProjectsCell:UICollectionViewCell
 {
-    weak var label:UILabel!
+    private weak var label:UILabel!
+    private weak var labelDistance:UILabel!
+    private weak var labelPoints:UILabel!
+    private let numberFormatter:NumberFormatter
+    private let kMaxDecimal:Int = 3
     
     override init(frame:CGRect)
     {
+        numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        numberFormatter.maximumFractionDigits = kMaxDecimal
+        
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor(white:1, alpha:0.5)
@@ -18,20 +26,35 @@ class VProjectsCell:UICollectionViewCell
         label.textColor = UIColor.main
         self.label = label
         
+        let labelPoints:UILabel = UILabel()
+        labelPoints.isUserInteractionEnabled = false
+        labelPoints.translatesAutoresizingMaskIntoConstraints = false
+        labelPoints.backgroundColor = UIColor.clear
+        labelPoints.font = UIFont.medium(size:14)
+        labelPoints.textColor = UIColor.main
+        self.labelPoints = labelPoints
+        
         addSubview(label)
+        addSubview(labelPoints)
         
         let views:[String:UIView] = [
-            "label":label]
+            "label":label,
+            "labelPoints":labelPoints]
         
         let metrics:[String:CGFloat] = [:]
         
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"H:|-10-[label(250)]",
+            withVisualFormat:"H:|-10-[label(200)]",
             options:[],
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-0-[label]-0-|",
+            withVisualFormat:"H:|-10-[labelPoints(200)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-10-[label(20)]-0-[labelPoints(17)]",
             options:[],
             metrics:metrics,
             views:views))
@@ -47,5 +70,22 @@ class VProjectsCell:UICollectionViewCell
     func config(model:MProjectsItem)
     {
         label.text = model.name
+        
+        guard
+            
+            let locations:[MProjectItemLocation] = model.locations,
+            let pointsString:String = numberFormatter.string(
+                from:locations.count as NSNumber)
+        
+        else
+        {
+            return
+        }
+        
+        let pointsCompositeString:String = String(
+            format:NSLocalizedString("VProjectsCell_points", comment:""),
+            pointsString)
+        
+        labelPoints.text = pointsCompositeString
     }
 }
