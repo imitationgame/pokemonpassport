aad439c15e26ef51ffc8c4f8a72f063ba4fc3a25
vauxhall <zero.griffin@gmail.com>
2016-12-18 14:26:26 -0600

    label distance

--- pokepass/View/Projects/VProjectsCell.swift
+++ pokepass/View/Projects/VProjectsCell.swift
@@ -1,12 +1,16 @@
 import UIKit
+import CoreLocation
 
 class VProjectsCell:UICollectionViewCell
 {
     private weak var label:UILabel!
     private weak var labelDistance:UILabel!
     private weak var labelPoints:UILabel!
+    private weak var model:MProjectsItem?
     private let numberFormatter:NumberFormatter
     private let kMaxDecimal:Int = 3
+    private let kKiloMeter:Double = 0.001
+    private let kMile:Double = 0.000621371
     
     override init(frame:CGRect)
     {
@@ -16,13 +20,13 @@ class VProjectsCell:UICollectionViewCell
         
         super.init(frame:frame)
         clipsToBounds = true
-        backgroundColor = UIColor(white:1, alpha:0.5)
+        backgroundColor = UIColor(white:1, alpha:0.9)
         
         let label:UILabel = UILabel()
         label.isUserInteractionEnabled = false
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = UIColor.clear
-        label.font = UIFont.medium(size:16)
+        label.font = UIFont.medium(size:17)
         label.textColor = UIColor.main
         self.label = label
         
@@ -30,16 +34,26 @@ class VProjectsCell:UICollectionViewCell
         labelPoints.isUserInteractionEnabled = false
         labelPoints.translatesAutoresizingMaskIntoConstraints = false
         labelPoints.backgroundColor = UIColor.clear
-        labelPoints.font = UIFont.medium(size:14)
-        labelPoints.textColor = UIColor.main
+        labelPoints.font = UIFont.medium(size:13)
+        labelPoints.textColor = UIColor.black
         self.labelPoints = labelPoints
+        
+        let labelDistance:UILabel = UILabel()
+        labelDistance.isUserInteractionEnabled = false
+        labelDistance.translatesAutoresizingMaskIntoConstraints = false
+        labelDistance.backgroundColor = UIColor.clear
+        labelDistance.font = UIFont.regular(size:13)
+        labelDistance.textColor = UIColor.black
+        self.labelDistance = labelDistance
         
         addSubview(label)
         addSubview(labelPoints)
+        addSubview(labelDistance)
         
         let views:[String:UIView] = [
             "label":label,
-            "labelPoints":labelPoints]
+            "labelPoints":labelPoints,
+            "labelDistance":labelDistance]
         
         let metrics:[String:CGFloat] = [:]
         
@@ -54,7 +68,12 @@ class VProjectsCell:UICollectionViewCell
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-10-[label(20)]-0-[labelPoints(17)]",
+            withVisualFormat:"H:|-10-[labelDistance(200)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-10-[label(21)]-0-[labelPoints(17)]-0-[labelDistance(17)]",
             options:[],
             metrics:metrics,
             views:views))
@@ -65,15 +84,90 @@ class VProjectsCell:UICollectionViewCell
         fatalError()
     }
     
+    //MARK: private
+    
+    private func computeDistance()
+    {
+        guard
+            
+            let locations:NSOrderedSet = model?.model.projectLocations
+            
+        else
+        {
+            return
+        }
+        
+        var amount:Double = 0
+        var prevLocation:CLLocation?
+        
+        for item:Any in locations
+        {
+            guard
+            
+                let dbItem:DObjectLocation = item as? DObjectLocation
+            
+            else
+            {
+                continue
+            }
+            
+            let location:CLLocation = CLLocation(
+                latitude:dbItem.latitude,
+                longitude:dbItem.longitude)
+            
+            if let previous:CLLocation = prevLocation
+            {
+                amount += previous.distance(from:location)
+            }
+            
+            prevLocation = location
+        }
+        
+        let distanceShort:Double
+        let name:String
+        
+        if MSettings.sharedInstance.model?.measures == MSettings.Measures.metric.rawValue
+        {
+            name = NSLocalizedString("VProjectsCell_km", comment:"")
+            distanceShort = amount * kKiloMeter
+        }
+        else
+        {
+            name = NSLocalizedString("VProjectsCell_mile", comment:"")
+            distanceShort = amount * kMile
+        }
+        
+        guard
+        
+            let distanceString:String = numberFormatter.string(
+                from:distanceShort as NSNumber)
+        
+        else
+        {
+            return
+        }
+        
+        let distanceComposite:String = String(
+            format:name,
+            distanceString)
+        
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.labelDistance.text = distanceComposite
+        }
+    }
+    
     //MARK: public
     
     func config(model:MProjectsItem)
     {
+        self.model = model
         label.text = model.name
         
         guard
             
-            let locations:[MProjectItemLocation] = model.locations,
+            let locations:NSOrderedSet = model.model.projectLocations,
             let pointsString:String = numberFormatter.string(
                 from:locations.count as NSNumber)
         
@@ -87,5 +181,11 @@ class VProjectsCell:UICollectionViewCell
             pointsString)
         
         labelPoints.text = pointsCompositeString
+        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.computeDistance()
+        }
     }
 }
